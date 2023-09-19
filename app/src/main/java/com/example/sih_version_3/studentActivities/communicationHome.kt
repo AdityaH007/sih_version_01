932ee7bf05d10ecb686f95fc72a50cb561163919
Aditya Kumar Saini <adityasainics10@gmail.com>
2023-09-19 18:51:53 +0530

    first commit

---
+++ app/src/main/java/com/example/sih_version_3/studentActivities/communicationHome.kt
@@ -0,0 +1,34 @@
+package com.example.sih_version_3.studentActivities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sih_version_3.R
+import com.example.sih_version_3.databinding.ActivityCommunicationHomeBinding
+
+class communicationHome : AppCompatActivity() {
+    private lateinit var binding: ActivityCommunicationHomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityCommunicationHomeBinding.inflate(layoutInflater)
+
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+     binding.speechRecognition.setOnClickListener {
+         startActivity(Intent(this,improveYourSpeech::class.java))
+     }
+
+        binding.mentorEnglish.setOnClickListener {
+            startActivity(Intent(this,communicationMentorList::class.java))
+
+
+        }
+
+        binding.quizTaker.setOnClickListener {
+            startActivity(Intent(this,engllishQuiz::class.java))
+        }
+
+
+    }
+}
